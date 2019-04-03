37d85185175b0caedfc2b7f3a2fc266672a13db5
Fabrizio Destro <destro.fabrizio@gmail.com>
2019-04-03 23:00:18 +0200

    Added AlertDialog custom style.

--- app/src/main/java/me/destro/android/playground/MainActivity.kt
+++ app/src/main/java/me/destro/android/playground/MainActivity.kt
@@ -3,6 +3,7 @@ package me.destro.android.playground
 import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import me.destro.playground.android.databinding.ActivityMainBinding
 import me.destro.android.playground.reactivex.ObservableErrors
@@ -14,6 +15,16 @@ class MainActivity : AppCompatActivity() {
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,
             R.layout.activity_main
         )
+
+        binding.alertDialog.setOnClickListener {
+            val alertDialog = AlertDialog.Builder(this)
+                .setTitle("Titolo")
+                .setMessage("Messaggio")
+                .setPositiveButton("Ok", null)
+                .setNegativeButton("No", null)
+                .setNeutralButton("Neutral", null)
+                .show()
+        }
 
         binding.observableErrors.setOnClickListener {
             val intent = Intent(this, ObservableErrors::class.java)
