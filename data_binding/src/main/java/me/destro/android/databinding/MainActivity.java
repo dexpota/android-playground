655d07c959dd25e7f1e66cca6fa9d385b814b2fa
Fabrizio Destro <destro.fabrizio@gmail.com>
2019-04-22 19:23:58 +0200

    Modified data binding main activity to display a list of available examples.

--- data_binding/src/main/java/me/destro/android/databinding/MainActivity.java
+++ data_binding/src/main/java/me/destro/android/databinding/MainActivity.java
@@ -1,56 +1,25 @@
 package me.destro.android.databinding;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
-import me.destro.android.databinding.model.ObservableUser;
-import me.destro.android.databinding.databinding.ActivityMainBinding;
-import me.destro.android.databinding.model.User;
-import me.destro.android.databinding.reactions.UserReaction;
-import org.joda.time.LocalDateTime;
-
-import java.util.Arrays;
-import java.util.List;
+import android.support.v7.app.AppCompatActivity;
+import me.destro.android.databinding.basic.BasicActivity;
+import me.destro.android.databinding.databinding.ActivityExamplesBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        ActivityExamplesBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_examples);
 
-        // TODO move this into a repo
-        ArchitectureApplication.Companion.getTypicodeApi().users()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe((users) -> {
-                    if (users.size() > 0)
-                        binding.setUser(convert(users.get(0)));
-                });
 
-        List<String> hobbies = Arrays.asList(null, "elettronica", "informatica");
-        // In this case if hobbies is null nothing bad happens.
-        LocalDateTime now = LocalDateTime.now();
-        User user = new User(0,"Fabrizio", "Destro", null, now.toDate(), hobbies);
-        UserReaction reaction = new UserReaction();
+        binding.basic.getRoot().setOnClickListener(view -> {
+            Intent intent = new Intent(this, BasicActivity.class);
+            startActivity(intent);
+        });
 
-        ObservableUser observableUser = new ObservableUser();
-        observableUser.setFirstName("Tizio");
-        observableUser.setLastName("Caio");
-
-        // If the value is null, is not a problem. In this case no values will be displayed.
-        //binding.setUser(null);
-
-        binding.setUserReaction(reaction);
-        binding.setUser(user);
-        binding.setObservableUser(observableUser);
-    }
-
-    // TODO this is a data mapping functionality, move it from here
-    private User convert(me.destro.android.core.api.typicode.model.User user) {
-        LocalDateTime now = LocalDateTime.now();
-        return new User(user.getId(), user.getName(), "Missing", user.getUsername(), now.toDate(), null);
+        binding.basic.title.setText(R.string.basic_example_title);
     }
 }
---
+++ data_binding/src/main/java/me/destro/android/databinding/basic/BasicActivity.java
@@ -0,0 +1,58 @@
+package me.destro.android.databinding.basic;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import me.destro.android.databinding.ArchitectureApplication;
+import me.destro.android.databinding.R;
+import me.destro.android.databinding.databinding.ActivityBasicBinding;
+import me.destro.android.databinding.model.ObservableUser;
+import me.destro.android.databinding.model.User;
+import me.destro.android.databinding.reactions.UserReaction;
+import org.joda.time.LocalDateTime;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BasicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityBasicBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_basic);
+
+        // TODO move this into a repo
+        ArchitectureApplication.Companion.getTypicodeApi().users()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe((users) -> {
+                    if (users.size() > 0)
+                        binding.setUser(convert(users.get(0)));
+                });
+
+        List<String> hobbies = Arrays.asList(null, "elettronica", "informatica");
+        // In this case if hobbies is null nothing bad happens.
+        LocalDateTime now = LocalDateTime.now();
+        User user = new User(0,"Fabrizio", "Destro", null, now.toDate(), hobbies);
+        UserReaction reaction = new UserReaction();
+
+        ObservableUser observableUser = new ObservableUser();
+        observableUser.setFirstName("Tizio");
+        observableUser.setLastName("Caio");
+
+        // If the value is null, is not a problem. In this case no values will be displayed.
+        //binding.setUser(null);
+
+        binding.setUserReaction(reaction);
+        binding.setUser(user);
+        binding.setObservableUser(observableUser);
+    }
+
+    // TODO this is a data mapping functionality, move it from here
+    private User convert(me.destro.android.core.api.typicode.model.User user) {
+        LocalDateTime now = LocalDateTime.now();
+        return new User(user.getId(), user.getName(), "Missing", user.getUsername(), now.toDate(), null);
+    }
+}
